2ae12187860c7a7352de65d9f793b606ca60e97d
Sergey Kurdamosov <gd_mops@mail.ru>
2013-05-06 21:18:25 +0300

    Убрал лишку. Начинаю подргужать димин файл

--- mops_diplom/glw.cpp
+++ mops_diplom/glw.cpp
@@ -33,122 +33,16 @@ GLW::GLW()
    // QString str = QFileDialog::getOpenFileName(0,"OpenFile"," ","*.txt");
 
 
-    QFile file("C://res//textur1.txt");
+    QFile file("qwe.txt");
 
     if(!file.open(QIODevice::ReadOnly))
     {
-        qDebug() << "Ошибка открытия для чтения";
+        qDebug() << "Error read";
         textur1 = "C:\\image\\no_images.jpg";
     }
     textur1 = file.readAll();
+    qDebug() << textur1;
 
-    QFile file2("C://res//textur2.txt");
-
-    if(!file2.open(QIODevice::ReadOnly))
-    {
-        qDebug() << "Ошибка открытия для чтения";
-        textur2 = "C:\\image\\no_images.jpg";
-    }
-    textur2 = file2.readAll();
-
-    QFile file3("C://res//textur3.txt");
-
-    if(!file3.open(QIODevice::ReadOnly))
-    {
-        qDebug() << "Ошибка открытия для чтения";
-        textur3 = "C:\\image\\no_images.jpg";
-    }
-    textur3 = file3.readAll();
-
-    QFile file4("C://res//textur4.txt");
-
-    if(!file4.open(QIODevice::ReadOnly))
-    {
-        qDebug() << "Ошибка открытия для чтения";
-        textur4 = "C:\\image\\no_images.jpg";
-    }
-    textur4 = file4.readAll();
-
-    QFile file5("C://res//textur5.txt");
-
-    if(!file5.open(QIODevice::ReadOnly))
-    {
-        qDebug() << "Ошибка открытия для чтения";
-        textur5 = "C:\\image\\no_images.jpg";
-    }
-    textur5 = file5.readAll();
-
-    QFile file6("C://res//textur6.txt");
-
-    if(!file6.open(QIODevice::ReadOnly))
-    {
-        qDebug() << "Ошибка открытия для чтения";
-        textur6 = "C:\\image\\no_images.jpg";
-    }
-    textur6 = file6.readAll();
-
-    QFile file7("C://res//textur7.txt");
-
-    if(!file7.open(QIODevice::ReadOnly))
-    {
-        qDebug() << "Ошибка открытия для чтения";
-        textur7 = "C:\\image\\no_images.jpg";
-    }
-    textur7 = file7.readAll();
-
-    QFile file8("C://res//textur8.txt");
-
-    if(!file8.open(QIODevice::ReadOnly))
-    {
-        qDebug() << "Ошибка открытия для чтения";
-        textur8 = "C:\\image\\no_images.jpg";
-    }
-    textur8 = file8.readAll();
-
-    QFile file9("C://res//textur9.txt");
-
-    if(!file9.open(QIODevice::ReadOnly))
-    {
-        qDebug() << "Ошибка открытия для чтения";
-        textur9 = "C:\\image\\no_images.jpg";
-    }
-    textur9 = file9.readAll();
-
-    QFile file10("C://res//textur10.txt");
-
-    if(!file10.open(QIODevice::ReadOnly))
-    {
-        qDebug() << "Ошибка открытия для чтения";
-        textur10 = "C:\\image\\no_images.jpg";
-    }
-    textur10 = file10.readAll();
-
-    QFile file11("C://res//textur11.txt");
-
-    if(!file11.open(QIODevice::ReadOnly))
-    {
-        qDebug() << "Ошибка открытия для чтения";
-        textur11 = "C:\\image\\no_images.jpg";
-    }
-    textur11 = file11.readAll();
-
-    QFile file12("C://res//textur12.txt");
-
-    if(!file12.open(QIODevice::ReadOnly))
-    {
-        qDebug() << "Ошибка открытия для чтения";
-        textur12 = "C:\\image\\no_images.jpg";
-    }
-    textur12 = file12.readAll();
-
-    QFile file13("C://res//textur13.txt");
-
-    if(!file13.open(QIODevice::ReadOnly))
-    {
-        qDebug() << "Ошибка открытия для чтения";
-        textur13 = "C:\\image\\no_images.jpg";
-    }
-    textur13 = file13.readAll();
 
 ///////////////////////.///////////////////////.///////////////////////.///////////////////////.
    // QMessageBox::information(0, s1);
@@ -172,8 +66,6 @@ void GLW::initializeGL()
    // glEnable(GL_CULL_FACE);
 
     startTextur();
-
-
 }
 
 void GLW::paintGL()
@@ -200,8 +92,6 @@ void GLW::paintGL()
      glTranslatef(xTra, yTra, zTra);
 
      glEnable( GL_CULL_FACE );
-
-
      glColor3ub( 139, 58 ,58  );
      glBegin(GL_POLYGON);
        glVertex3f(-10,0,-10);
@@ -211,7 +101,7 @@ void GLW::paintGL()
      glEnd();
      glDisable( GL_CULL_FACE );
 
-     glBindTexture(GL_TEXTURE_2D, textureID[0]);
+     /*glBindTexture(GL_TEXTURE_2D, textureID[0]);
      //01
      glBegin(GL_POLYGON);
      glColor3ub( 255.0f,255.0f,255.0f );
@@ -224,214 +114,8 @@ void GLW::paintGL()
      glTexCoord2f(1,0);
      glVertex3f(-8,0.2f,-10);
      glEnd();
-     glBindTexture(GL_TEXTURE_2D, textureID[2]);
-     //02
-     glBegin(GL_POLYGON);
-     glColor3ub( 255.0f,255.0f,255.0f );
-     glTexCoord2f(0,0);
-     glVertex3f(-7,0.2f,-10);
-     glTexCoord2f(0,1);
-     glVertex3f(-7,1,-10);
-     glTexCoord2f(1,1);
-     glVertex3f(-6,1,-10);
-     glTexCoord2f(1,0);
-     glVertex3f(-6,0.2f,-10);
-     glEnd();
+     glBindTexture(GL_TEXTURE_2D, textureID[2]);*/
 
-     glBindTexture(GL_TEXTURE_2D, textureID[3]);
-     //03
-     glBegin(GL_POLYGON);
-     glColor3ub( 255.0f,255.0f,255.0f );
-     glTexCoord2f(0,0);
-     glVertex3f(-5,0.2f,-10);
-     glTexCoord2f(0,1);
-     glVertex3f(-5,1,-10);
-     glTexCoord2f(1,1);
-     glVertex3f(-4,1,-10);
-     glTexCoord2f(1,0);
-     glVertex3f(-4,0.2f,-10);
-     glEnd();
-
-     glBindTexture(GL_TEXTURE_2D, textureID[4]);
-     //04
-     glBegin(GL_POLYGON);
-     glColor3ub( 255.0f,255.0f,255.0f );
-     glTexCoord2f(0,0);
-     glVertex3f(-3,0.2f,-10);
-     glTexCoord2f(0,1);
-     glVertex3f(-3,2,-10);
-     glTexCoord2f(1,1);
-     glVertex3f(0,2,-10);
-     glTexCoord2f(1,0);
-     glVertex3f(0,0.2f,-10);
-     glEnd();
-
-     glBindTexture(GL_TEXTURE_2D, textureID[5]);
-     //05
-     glBegin(GL_POLYGON);
-     glColor3ub( 255.0f,255.0f,255.0f );
-     glTexCoord2f(0,0);
-     glVertex3f(1,0.2f,-10);
-     glTexCoord2f(0,1);
-     glVertex3f(1,1,-10);
-     glTexCoord2f(1,1);
-     glVertex3f(2,1,-10);
-     glTexCoord2f(1,0);
-     glVertex3f(2,0.2f,-10);
-     glEnd();
-
-     glBindTexture(GL_TEXTURE_2D, textureID[6]);
-     //06
-     glBegin(GL_POLYGON);
-     glColor3ub( 255.0f,255.0f,255.0f );
-     glTexCoord2f(0,0);
-     glVertex3f(3,0.2f,-10);
-     glTexCoord2f(0,1);
-     glVertex3f(3,2,-10);
-     glTexCoord2f(1,1);
-     glVertex3f(7,2,-10);
-     glTexCoord2f(1,0);
-     glVertex3f(7,0.2f,-10);
-     glEnd();
-
-     glBindTexture(GL_TEXTURE_2D, textureID[7]);
-     //07
-     glBegin(GL_POLYGON);
-     glColor3ub( 255.0f,255.0f,255.0f );
-     glTexCoord2f(0,0);
-     glVertex3f(-9,0.2f,0);
-     glTexCoord2f(0,1);
-     glVertex3f(-9,1,0);
-     glTexCoord2f(1,1);
-     glVertex3f(-8,1,0);
-     glTexCoord2f(1,0);
-     glVertex3f(-8,0.2f,0);
-     glEnd();
-
-     glBindTexture(GL_TEXTURE_2D, textureID[8]);
-     //08
-     glBegin(GL_POLYGON);
-     glColor3ub( 255.0f,255.0f,255.0f );
-     glTexCoord2f(0,0);
-     glVertex3f(-7,0.2f,0);
-     glTexCoord2f(0,1);
-     glVertex3f(-7,2,0);
-     glTexCoord2f(1,1);
-     glVertex3f(-5,2,0);
-     glTexCoord2f(1,0);
-     glVertex3f(-5,0.2f,0);
-     glEnd();
-
-     glBindTexture(GL_TEXTURE_2D, textureID[9]);
-     //09
-     glBegin(GL_POLYGON);
-     glColor3ub( 255.0f,255.0f,255.0f );
-     glTexCoord2f(0,0);
-     glVertex3f(-3,0.2f,0);
-     glTexCoord2f(0,1);
-     glVertex3f(-3,1,0);
-     glTexCoord2f(1,1);
-     glVertex3f(-1,1,0);
-     glTexCoord2f(1,0);
-     glVertex3f(-1,0.2f,0);
-     glEnd();
-
-     glBindTexture(GL_TEXTURE_2D, textureID[10]);
-     //10
-     glBegin(GL_POLYGON);
-     glColor3ub( 255.0f,255.0f,255.0f );
-     glTexCoord2f(0,0);
-     glVertex3f(0,0.2f,0);
-     glTexCoord2f(0,1);
-     glVertex3f(0,1,0);
-     glTexCoord2f(1,1);
-     glVertex3f(2,1,0);
-     glTexCoord2f(1,0);
-     glVertex3f(2,0.2f,0);
-     glEnd();
-
-     glBindTexture(GL_TEXTURE_2D, textureID[11]);
-     //07
-     glBegin(GL_POLYGON);
-     glColor3ub( 255.0f,255.0f,255.0f );
-     glTexCoord2f(0,0);
-     glVertex3f(3,0.2f,0);
-     glTexCoord2f(0,1);
-     glVertex3f(3,1,0);
-     glTexCoord2f(1,1);
-     glVertex3f(4,1,0);
-     glTexCoord2f(1,0);
-     glVertex3f(4,0.2f,0);
-     glEnd();
-
-     glBindTexture(GL_TEXTURE_2D, textureID[12]);
-     //07
-     glBegin(GL_POLYGON);
-     glColor3ub( 255.0f,255.0f,255.0f );
-     glTexCoord2f(0,0);
-     glVertex3f(5,0.2f,0);
-     glTexCoord2f(0,1);
-     glVertex3f(5,1.5f,0);
-     glTexCoord2f(1,1);
-     glVertex3f(6,1.5f,0);
-     glTexCoord2f(1,0);
-     glVertex3f(6,0.2f,0);
-     glEnd();
-
-     glBindTexture(GL_TEXTURE_2D, textureID[13]);
-     //07
-     glBegin(GL_POLYGON);
-     glColor3ub( 255.0f,255.0f,255.0f );
-     glTexCoord2f(0,0);
-     glVertex3f(7,0.2f,0);
-     glTexCoord2f(0,1);
-     glVertex3f(7,2,0);
-     glTexCoord2f(1,1);
-     glVertex3f(8,2,0);
-     glTexCoord2f(1,0);
-     glVertex3f(8,0.2f,0);
-     glEnd();
-
-     glBegin(GL_POLYGON);
-
-     glColor3ub( 245.0f,255.0f,250.0f );
-     glVertex3f(-10,0,-10.1);
-     glVertex3f(-10,5,-10.1);
-     glVertex3f(10,5,-10.1);
-     glVertex3f(10,0,-10.1);
-     glEnd();
-
-     glBegin(GL_POLYGON);
-     glColor3ub( 245.0f,255.0f,250.0f );
-     glVertex3f(10,0,-10);
-     glVertex3f(10,5,-10);
-     glVertex3f(10,5,0);
-     glVertex3f(10,0,0);
-     glEnd();
-
-     glBegin(GL_POLYGON);
-     glColor3ub( 245.0f,255.0f,250.0f );
-     glVertex3f(10,0,0.1);
-     glVertex3f(10,5,0.1);
-     glVertex3f(-10,5,0.1);
-     glVertex3f(-10,0,0.1);
-     glEnd();
-
-     glBegin(GL_POLYGON);
-     glColor3ub( 245.0f,255.0f,250.0f );
-     glVertex3f(-10,0,0);
-     glVertex3f(-10,5,0);
-     glVertex3f(-10,5,-10);
-     glVertex3f(-10,0,-10);
-     glEnd();
-
-     glBegin(GL_POLYGON);
-     glColor3ub( 255.0f,248.0f,220.0f );
-     glVertex3f(-10,5,-10);
-     glVertex3f(10,5,-10);
-     glVertex3f(10,5,0);
-     glVertex3f(-10,5,0);
-     glEnd();
 
 
  }
